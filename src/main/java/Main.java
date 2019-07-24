e225c200483276fc806da8e08810731d26705a0d
rimashahbazyan <s.rima2000rs@gmail.com>
2019-07-24 14:17:05 +0400

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.lang.reflect.Array;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,51 +18,96 @@ public class Main {
 
 
     public static void main(String[] args) {
+        String command;
+        try (
+                Scanner Sc = new Scanner(System.in);
 
-        ArrayList<Doctor> AllDocs= new ArrayList<>();
-        ArrayList<Patient> AllPats =  new ArrayList<>();
-        try {
-            ObjectMapper op = new ObjectMapper();
-            FileReader frDocs = new FileReader("Docs.json");
-            FileReader frPats = new FileReader("Pats.json");
+                Connection connection = DriverManager.getConnection(
+                        "jdbc:sqlserver://localhost:1433;database=MyDB",
+                        "sa",
+                        "1111"
+                        );
+                )
+        {
 
-            // restore from JSON
+            String query;
+//            PreparedStatement statement= null;
+//            query = "CREATE TABLE Patient( ID INT PRIMARY KEY IDENTITY, NAME VARCHAR(100), SURNAME VARCHAR(100) )";
+//            statement = connection.prepareStatement(query);
+//            statement.execute();
+            PreparedStatement statement= null;
+            System.out.println("Write the command : ");
+            command = Sc.nextLine().toUpperCase();
+            while(!command.equals("EXIT")) {
+                String[] param = Sc.nextLine().split(" ");
+                switch (command) {
+                    case "INSERT":
 
-            AllDocs = new ArrayList<>( Arrays.asList((op.readValue(frDocs,  Doctor[].class))));
-            AllPats = new ArrayList<>( Arrays.asList((op.readValue(frPats,  Patient[].class))));
-
-
-            Scanner Sc = new Scanner(System.in);
-            System.out.print("Enter the command(Add, Remove, Print) :   ");
-            String command = Sc.nextLine();
-            System.out.print("Who do you want to " + command +"(ipf: role name surname) :   ");
-            String[] smo = Sc.nextLine().split(" ");
-
-            if(command.equals( "Add"))
-            {
-                switch (smo[0]) {
-                    case "Doctor":
-                        AllDocs.add( new Doctor(smo[1], smo[2], "null")); break;
-                    case "Patient":
-                        AllPats.add( new Patient(smo[1], smo[2], "null")); break;
+                        query = "INSERT INTO "+param[0]+"(name,surname) VALUES(?,?)";
+                        statement = connection.prepareStatement(query);
+                        statement.setString(1,param[1]);
+                        statement.setString(2,param[2]);
+                        statement.execute();
+                        break;
+                    case "DELETE":
+                        query = "DELETE  from "+param[0]+" where name = ? and surname = ?";
+                        statement = connection.prepareStatement(query);
+                        statement.setString(1,param[1]);
+                        statement.setString(2,param[2]);
+                        statement.execute();
+                        break;
                 }
-
-            }
-            else if( command.equals( "Remove"))
-            {
-
-            }
-            else
-            {
-
+                command = Sc.nextLine().toUpperCase();
             }
 
-
-            op.writeValue(new File("Docs.json"), AllDocs);
-            op.writeValue(new File("Pats.json"), AllPats);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        System.out.println();
+
+//        ArrayList<Doctor> AllDocs= new ArrayList<>();
+//        ArrayList<Patient> AllPats =  new ArrayList<>();
+//        try {
+//            ObjectMapper op = new ObjectMapper();
+//            FileReader frDocs = new FileReader("Docs.json");
+//            FileReader frPats = new FileReader("Pats.json");
+//
+//            // restore from JSON
+//
+//            AllDocs = new ArrayList<>( Arrays.asList((op.readValue(frDocs,  Doctor[].class))));
+//            AllPats = new ArrayList<>( Arrays.asList((op.readValue(frPats,  Patient[].class))));
+//
+//
+//
+//            System.out.print("Enter the command(Add, Remove, Print) :   ");
+//            command = Sc.nextLine();
+//            System.out.print("Who do you want to " + command +"(ipf: role name surname) :   ");
+//            String[] smo = Sc.nextLine().split(" ");
+//
+//            if(command.equals( "Add"))
+//            {
+//                switch (smo[0]) {
+//                    case "Doctor":
+//                        AllDocs.add( new Doctor(smo[1], smo[2], "null")); break;
+//                    case "Patient":
+//                        AllPats.add( new Patient(smo[1], smo[2], "null")); break;
+//                }
+//
+//            }
+//            else if( command.equals( "Remove"))
+//            {
+//
+//            }
+//            else
+//            {
+//
+//            }
+//
+//
+//            op.writeValue(new File("Docs.json"), AllDocs);
+//            op.writeValue(new File("Pats.json"), AllPats);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println();
     }
 }
